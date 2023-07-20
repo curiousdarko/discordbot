10464442aba55a8b154810f9fcc25cdd02d3bee1
curiousdarko <133599091+curiousdarko@users.noreply.github.com>
2023-07-20 19:10:17 +0000

    Create runthis.bat

---
+++ runthis.bat
@@ -0,0 +1,11 @@
+@echo on
+echo Press any button to run the program
+pause>nul
+pip install -r requirements.txt
+echo All requirements installed
+echo Press any button to continue to the main program (you have to have had put in your token.)
+pause>nul
+code.py
+echo Something went wrong if you're here, please copy and paste the text above in my comments and I'll help.
+echo Press any key to exit the program
+pause>nul
