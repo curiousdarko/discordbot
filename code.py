df0afa355d17f6deb626480bca0ebe9f754fc398
curiousdarko <133599091+curiousdarko@users.noreply.github.com>
2023-07-20 19:06:29 +0000

    Update code.py

--- code.py
+++ code.py
@@ -1,35 +1,46 @@
 import discord
 import random
-from discord_components import DiscordComponents, Button, ButtonStyle
+from discord_slash import SlashCommand, SlashContext
 
 client = discord.Client()
-DiscordComponents(client)
+slash = SlashCommand(client, sync_commands=True)
 
 @client.event
 async def on_ready():
     print('Bot is ready.')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('!fact'):
-        fact = get_random_fact()
-        embed = discord.Embed(title="Random Fact", description=fact, color=discord.Color.blue())
-        await message.channel.send(embed=embed, components=[Button(style=ButtonStyle.blue, label="Get another fact", custom_id="get_fact")])
+@slash.slash(name="fact", description="Get a random fact")
+async def get_fact(ctx: SlashContext):
+    fact = get_random_fact()
+    embed = discord.Embed(title="Random Fact", description=fact, color=discord.Color.blue())
+    await ctx.send(embed=embed, components=[create_button()])
 
 @client.event
-async def on_button_click(interaction):
-    if interaction.custom_id == "get_fact":
+async def on_component(ctx):
+    if ctx.component_id == "get_fact":
         fact = get_random_fact()
         embed = discord.Embed(title="Random Fact", description=fact, color=discord.Color.blue())
-        await interaction.message.edit(embed=embed)
+        await ctx.edit_origin(embed=embed, components=[create_button()])
 
 def get_random_fact():
     with open('facts.txt', 'r') as file:
         facts = file.readlines()
         fact = random.choice(facts).strip()
     return fact
+
+def create_button():
+    return [
+        {
+            "type": 1,
+            "components": [
+                {
+                    "type": 2,
+                    "label": "Get another fact",
+                    "style": 1,
+                    "custom_id": "get_fact"
+                }
+            ]
+        }
+    ]
 
 client.run('YOUR_DISCORD_TOKEN')
