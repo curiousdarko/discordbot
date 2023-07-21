0d0a1842627815bbc9f11a4292c5c6296f31b42c
curiousdarko <133599091+curiousdarko@users.noreply.github.com>
2023-07-21 10:43:20 +0000

    Update code.py

--- code.py
+++ code.py
@@ -1,34 +1,21 @@
+# youtube.com/@developerlaborotory
 import discord
-import random
-from discord_interactions import client
-from discord_slash import SlashCommand, SlashContext
-from discord_slash.model import ButtonStyle
-from discord_slash.utils import manage_components
-from discord_slash.utils.manage_commands import create_option, create_choice
+from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.messages = True
-intents.components = True
 
-client = discord.Client(intents=intents)
-slash = SlashCommand(client, sync_commands=True)
+bot = commands.Bot(command_prefix="/", intents=intents) #change the command prefix if u want, for example if you make it ? the command wil now be ?fact instead of /fact
 
-@client.event
+@bot.event
 async def on_ready():
-    print('Bot is ready.')
+    print('We have liftoff!')
 
-@slash.slash(name="fact", description="Get a random fact")
-async def get_fact(ctx: SlashContext):
+@bot.command()
+async def fact(ctx):
     fact = get_random_fact()
     embed = discord.Embed(title="Random Fact", description=fact, color=discord.Color.blue())
-    await ctx.send(embed=embed, components=[create_button()])
-
-@client.event
-async def on_component(ctx):
-    if ctx.component_id == "get_fact":
-        fact = get_random_fact()
-        embed = discord.Embed(title="Random Fact", description=fact, color=discord.Color.blue())
-        await ctx.edit_origin(embed=embed, components=[create_button()])
+    await ctx.send(embed=embed)
 
 def get_random_fact():
     with open('facts.txt', 'r') as file:
@@ -36,13 +23,4 @@ def get_random_fact():
         fact = random.choice(facts).strip()
     return fact
 
-def create_button():
-    return manage_components.create_actionrow(
-        manage_components.create_button(
-            style=ButtonStyle.PRIMARY,
-            label="Get another fact",
-            custom_id="get_fact"
-        )
-    )
-
-client.run('YOUR_BOT_TOKEN')
+bot.run('YOUR_BOT_TOKEN')
