d45558e9cff077670b0e2b4fb7f99e27bb6b4e18
curiousdarko <133599091+curiousdarko@users.noreply.github.com>
2023-07-21 10:43:57 +0000

    Update runthis.bat

--- runthis.bat
+++ runthis.bat
@@ -1,11 +1,12 @@
-@echo on
+@echo off
 echo Press any button to run the program
-pause>nul
+pause
 pip install -r requirements.txt
 echo All requirements installed
-echo Press any button to continue to the main program (you have to have had put in your token.)
-pause>nul
+echo Press any button to continue to the main program (you have to have put in your token.)
+pause
 code.py
+color 4
 echo Something went wrong if you're here, please copy and paste the text above in my comments and I'll help.
 echo Press any key to exit the program
-pause>nul
+pause
